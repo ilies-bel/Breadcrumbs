77c8725b0beb7f951a43cd5b8c1261294fff8a18
RatumArch <ratum@protonmail.com>
2021-06-30 21:26:42 +0200

    Tables interview milestone dans quarkus

--- breadcrumbs-quarkus/src/main/java/apicore/entit/milestone/interview_milestones.java
+++ breadcrumbs-quarkus/src/main/java/apicore/entit/milestone/interview_milestones.java
@@ -1,10 +1,23 @@
 package apicore.entit.milestone;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
-public class interview_milestones extends PanacheEntity {
-    public String interview_process;
+public class interview_milestones extends PanacheEntityBase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer id_milestone;
+
+    public String interview_processes;
+
+    @ManyToOne
+    private interview_type types = new interview_type();
+
+    public String interview_type = types.title;
+
+    public String milestone_name;
+
 }
--- breadcrumbs-quarkus/src/main/java/apicore/entit/milestone/interview_type.java
+++ breadcrumbs-quarkus/src/main/java/apicore/entit/milestone/interview_type.java
@@ -1,14 +1,21 @@
 package apicore.entit.milestone;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import io.quarkus.runtime.annotations.RegisterForReflection;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
-public class interview_type extends PanacheEntity {
+public class interview_type extends PanacheEntityBase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id_interview_type;
+
     public String title;
     public String description;
-    public int time_length;
+    public Integer time_length;
     public String field;
 
     public static void setTime_length(int new_time) {
