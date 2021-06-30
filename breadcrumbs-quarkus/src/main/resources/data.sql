bf0852234a2dc88e8b554793c5ddf0d07dd681b1
RatumArch <ratum@protonmail.com>
2021-06-30 12:33:45 +0200

    data.sql

--- breadcrumbs-quarkus/src/main/resources/data.sql
+++ breadcrumbs-quarkus/src/main/resources/data.sql
@@ -1,5 +1,7 @@
-insert into interview_tips ("id", ranking, title, description) values (1, 1, 'Kiriga AB', 'Kenya');
-insert into interview_tips ("id", ranking, title, description) values (2, 2, 'Visit our website', '---');
-insert into interview_tips ("id", ranking, title, description) values (3, 2, 'Gather information', '---');
-insert into interview_tips ("id", ranking, title, description) values (4, 2, 'Gather information', 'Fuji');
-insert into interview_tips ("id", ranking, title, description) values (5, 2, 'Gather information', 'Redl');
+insert into interview_tips ("id", ranking, title, description) values (1, 2, 'Visit our website', '---');
+insert into interview_tips ("id", ranking, title, description) values (2, 2, 'Gather insight on the position', 'Go on our Career Page, blogs and social media and gather as much insights on the position you are applying to. Show us that you know what you are talking about');
+insert into interview_tips ("id", ranking, title, description) values (3, 2, 'Try to climb Redline', 'Redline is a really tall mountain.');
+insert into interview_tips ("id", ranking, title, description) values (4, 2, 'Prepare questions in advance', '---');
+insert into interview_tips ("id", ranking, title, description) values (5, 2, 'Gather your documents', '---');
+insert into interview_tips ("id", ranking, title, description) values (6, 2, 'Anticipate', '---');
+insert into interview_tips ("id", ranking, title, description) values (7, 2, 'Get in positive mindset', '---');
